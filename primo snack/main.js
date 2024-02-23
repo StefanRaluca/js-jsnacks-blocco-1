c4817d63b455ecbdd9b8fcfcf95474d7ddd4ffaf
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-23 12:18:55 +0100

    add snack 1

---
+++ primo snack/main.js
@@ -0,0 +1,15 @@
+console.log("it works");
+
+
+const user_number1 = prompt("Insert first number:");
+
+const user_number2 = prompt("Insert second number:");
+
+
+if (user_number1 > user_number2) {
+    console.log("User 1 it's first")
+} else if ( user_number2 > user_number1) {
+    console.log("User 1 it's first");
+} else {
+    console.log("The same number")
+}
