8edd99acb6cfc5a22f385ca695f4fe3700f986a9
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-26 10:54:04 +0100

    add snack 4 with for loop and while loop

---
+++ quarto snack/main.js
@@ -0,0 +1,30 @@
+console.log('work');
+
+
+/* let sum  =0;
+
+
+for (let i = 0; i < 10; i++) {
+   
+    let number = Number(prompt("Type a number"));
+ 
+    sum += number;
+}
+
+console.log("The sum of all entered numbers is:", sum);
+
+
+ */
+
+
+let sum = 0;
+let i = 0;
+
+while (i < 10) {
+    let number = Number(prompt("Type a number"));
+    sum += number;
+    i++;
+}
+
+
+console.log("The sum of all entered numbers is:", sum);
