c7a29568c840b2b4320501b88505607a638b71b2
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-26 12:22:41 +0100

    add snack6

---
+++ sesto snack/main.js
@@ -0,0 +1,12 @@
+console.log('work');
+
+
+let N = Number(prompt('Type a number'));
+
+    for (let i = 1; i <= N; i++) {
+        console.log(i ** 3); 
+    }
+
+
+    // let cubo = Math.pow(N,3)
+
